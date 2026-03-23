9459123eac61fc90a2832086d73590513e11f7d5
DenisAndrew77 <denis77andrew@gmail.com>
2026-03-23 16:37:06 +0200

    User repo and service

---
+++ user-service/src/main/java/com/ecommerce/user_service/service/UserService.java
@@ -0,0 +1,62 @@
+package com.ecommerce.user_service.service;
+
+import com.ecommerce.user_service.dto.UserDto;
+import com.ecommerce.user_service.dto.userBuilder.UserDtoBuilder;
+import com.ecommerce.user_service.entity.UserEntity;
+import com.ecommerce.user_service.exceptionHandling.exception.UserAlreadyExistsException;
+import com.ecommerce.user_service.exceptionHandling.exception.UserNotFoundException;
+import com.ecommerce.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserDto getById(Integer id) {
+        Optional<UserEntity> foundUser = userRepository.findById(id);
+        if(foundUser.isPresent()){
+            UserEntity user = foundUser.get();
+            UserDtoBuilder userBuilder = new UserDtoBuilder();
+            UserDto userToBeSent = userBuilder.setUsername(user.getUsername()).setEmail(user.getEmail()).setAge(user.getAge()).getUser();
+            return userToBeSent;
+        }
+        else throw new UserNotFoundException("User not found");
+    }
+
+    public UserDto getByUsername(String username) {
+        Optional<UserEntity> foundUser = userRepository.findByUsername(username);
+        if(foundUser.isPresent()){
+            UserEntity user = foundUser.get();
+            UserDtoBuilder userBuilder = new UserDtoBuilder();
+            UserDto userToBeSent = userBuilder.setUsername(user.getUsername()).setEmail(user.getEmail()).setAge(user.getAge()).getUser();
+            return userToBeSent;
+        }
+        else throw new UserNotFoundException("User not found");
+    }
+
+    public boolean createUser(UserEntity user) {
+        if(userRepository.findByUsername(user.getUsername()).isPresent()){
+            throw new UserAlreadyExistsException("Username already exists");
+        }
+        else {
+            userRepository.save(user);
+            return true;
+        }
+    }
+
+    public void deleteUser(Integer id) {
+        Optional<UserEntity> foundUser = userRepository.findById(id);
+        if(foundUser.isPresent()){
+            userRepository.deleteById(id);
+        }else throw new UserNotFoundException("User not found");
+    }
+}
