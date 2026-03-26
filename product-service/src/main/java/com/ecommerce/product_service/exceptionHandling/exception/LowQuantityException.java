bd5c25891755b83541337347942572689d55fbb7
DenisAndrew77 <denis77andrew@gmail.com>
2026-03-26 14:45:06 +0200

    Product Exception Handling

---
+++ product-service/src/main/java/com/ecommerce/product_service/exceptionHandling/exception/LowQuantityException.java
@@ -0,0 +1,11 @@
+package com.ecommerce.product_service.exceptionHandling.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class LowQuantityException extends RuntimeException {
+    public LowQuantityException(String message) {
+        super(message);
+    }
+}
--- product-service/src/main/java/com/ecommerce/product_service/exceptionHandling/exceptionHandler/ProductExceptionHandler.java
+++ product-service/src/main/java/com/ecommerce/product_service/exceptionHandling/exceptionHandler/ProductExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.ecommerce.product_service.exceptionHandling.exceptionHandler;
 
+import com.ecommerce.product_service.exceptionHandling.exception.LowQuantityException;
+import com.ecommerce.product_service.exceptionHandling.exception.ProductAlreadyExistsException;
 import com.ecommerce.product_service.exceptionHandling.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +13,22 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ProductExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception ex) {
+    public ResponseEntity<String> handleException(Exception ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException ex) {
+    public ResponseEntity<String> handleProductNotFoundException(ProductNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(LowQuantityException.class)
+    public ResponseEntity<String> handleLowQuantityException(LowQuantityException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(ProductAlreadyExistsException.class)
+    public ResponseEntity<String> handleProductAlreadyExistsException(ProductAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
     }
 }
