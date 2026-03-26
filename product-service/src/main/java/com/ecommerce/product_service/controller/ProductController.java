b0eb2722423002775fd01db234e8a198628b43a6
DenisAndrew77 <denis77andrew@gmail.com>
2026-03-26 14:45:35 +0200

    verifyAvailability route for product

--- product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
+++ product-service/src/main/java/com/ecommerce/product_service/controller/ProductController.java
@@ -5,9 +5,7 @@ import com.ecommerce.product_service.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -17,7 +15,20 @@ public class ProductController {
 
     @PostMapping("/product/create")
     public ResponseEntity<String> save(@RequestBody ProductDto product) {
-        productService.addNewProduct(product);
-        return new ResponseEntity<>("Product Created", HttpStatus.CREATED);
+        if(productService.addNewProduct(product)){
+            return new ResponseEntity<>("Product Created", HttpStatus.CREATED);
+        }else{
+            return new ResponseEntity<>("Product Not Created", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/product/{productName}/verifyAvailability/{requestedQuantity}")
+    public ResponseEntity<String> verifyAvailability(@PathVariable String productName, @PathVariable Integer requestedQuantity) {
+        if(productService.verifyProductAndAvailableQuantity(productName, requestedQuantity)){
+            return new ResponseEntity<>("Product:"+ productName +" is available", HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Product:"+ productName +" is not available", HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
+++ product-service/src/main/java/com/ecommerce/product_service/service/ProductService.java
@@ -2,10 +2,14 @@ package com.ecommerce.product_service.service;
 
 import com.ecommerce.product_service.dto.ProductDto;
 import com.ecommerce.product_service.entity.Product;
+import com.ecommerce.product_service.exceptionHandling.exception.LowQuantityException;
+import com.ecommerce.product_service.exceptionHandling.exception.ProductAlreadyExistsException;
 import com.ecommerce.product_service.exceptionHandling.exception.ProductNotFoundException;
 import com.ecommerce.product_service.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -20,11 +24,24 @@ public class ProductService {
     public boolean addNewProduct(ProductDto productDto) {
         Product product = new Product(productDto.getName(), productDto.getCategory(), productDto.getDescription(), productDto.getPrice(), productDto.getAvailableQuantity());
         if(productRepository.findByName(product.getName()).isPresent()) {
+            throw new ProductAlreadyExistsException("Product "+ productDto.getName()+" already exists");
+        }
+        else  {
             productRepository.save(product);
             return true;
         }
-        else  {
-            throw new ProductNotFoundException("Product not found");
+    }
+
+    public boolean verifyProductAndAvailableQuantity(String productName, Integer requestedQuantity) {
+        Optional<Product> product = productRepository.findByName(productName);
+        if(product.isPresent()) {
+            if(product.get().getAvailableQuantity() >= requestedQuantity) {
+                return true;
+            }
+            else {
+                throw new LowQuantityException("Low quantity");
+            }
         }
+        else throw new ProductNotFoundException("Product not found");
     }
 }
