27218174a5ea5bfea970171002c61712208ff21b
DenisAndrew77 <denis77andrew@gmail.com>
2026-04-07 14:32:29 +0300

    Refactor PaymentExceptionHandler: reorder import statements and update response statuses for LowBalanceException and AlreadyPaidException

--- payment-service/src/main/java/com/ecommerce/payment_service/exceptionHandling/exceptionHandler/PaymentExceptionHandler.java
+++ payment-service/src/main/java/com/ecommerce/payment_service/exceptionHandling/exceptionHandler/PaymentExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.ecommerce.payment_service.exceptionHandling.exceptionHandler;
 
-import com.ecommerce.payment_service.exceptionHandling.exception.AlreadyPaidException;
-import com.ecommerce.payment_service.exceptionHandling.exception.LowBalanceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.ecommerce.payment_service.exceptionHandling.exception.AlreadyPaidException;
+import com.ecommerce.payment_service.exceptionHandling.exception.LowBalanceException;
 
 @RestControllerAdvice
 public class PaymentExceptionHandler {
@@ -16,11 +17,11 @@ public class PaymentExceptionHandler {
 
     @ExceptionHandler(value = LowBalanceException.class)
     public ResponseEntity<String> handleLowBalanceException(LowBalanceException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.PAYMENT_REQUIRED);
     }
 
     @ExceptionHandler(value = AlreadyPaidException.class)
     public ResponseEntity<String> handleAlreadyPaidException(AlreadyPaidException exception) {
-        return new ResponseEntity<>(exception.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.CONFLICT);
     }
 }
